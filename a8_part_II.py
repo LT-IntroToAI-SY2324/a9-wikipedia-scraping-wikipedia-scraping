5cd6319169bc0afe6b18ef4d86a63125bc1765bf
Kelly Ocampo <kocampo13@cps.edu>
2024-05-16 10:28:24 -0500

    get density

--- a8_part_II.py
+++ a8_part_II.py
@@ -9,6 +9,24 @@ from utilities import *
 # Assignment 8 Part II
 
 
+def get_planet_mean_density(planet_name: str) -> str:
+    """Gets the mean density of the given planet
+
+    Args:
+        planet_name - name of the planet to get radius of
+
+    Returns:
+        mean_density of the given planet
+    """
+    infobox_text = clean_text(get_first_infobox_text(get_page_html(planet_name)))
+    # print(infobox_text)
+    # TODO: fill this in
+    pattern = "Mean density(?P<mean_density>[\d.]+)"
+    error_text = "Page infobox has no polar radius information"
+    match = get_match(infobox_text, pattern, error_text)
+    return match.group("mean_density")
+
+
 def get_planet_radius(planet_name: str) -> str:
     """Gets the radius of the given planet
 
